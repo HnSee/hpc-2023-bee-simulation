c4cb8a600f5bf20ccac7b479179e0941c8f831f0
HnSee <34213265+HnSee@users.noreply.github.com>
2023-06-08 16:45:13 +0200

    Add coordinate matrix get benchmark

--- benchmarks/multikey_matrix.cpp
+++ benchmarks/multikey_matrix.cpp
@@ -2,31 +2,9 @@
 
 #include "../src/utils/coordinate_matrix.hpp"
 #include <cstddef>
-#include <omp.h>
 #include <random>
 
 typedef unsigned char InnerMatrixType;
-
-template <typename T>
-static void constructRandomMatrix(CoordinateMatrix<T> &matrix,
-                                  std::size_t size) {
-  matrix = CoordinateMatrix<T>(size);
-
-  std::random_device dev;
-  std::mt19937 rng(dev());
-  std::uniform_int_distribution<T> uniform;
-
-  #pragma omp parallel for shared(matrix)
-  for (std::size_t i = 0; i < size; i++) {
-    for (std::size_t j = 0; j < size; j++) {
-      Coordinates newCoordinates = std::make_pair(i, j);
-      T randomValue = uniform(rng);
-      {
-      matrix.set(newCoordinates, randomValue);
-      }
-    }
-  }
-}
 
 static Coordinates constructRandomCoordinates(std::size_t min,
                                               std::size_t max) {
@@ -38,6 +16,22 @@ static Coordinates constructRandomCoordinates(std::size_t min,
   return randomCoordinate;
 }
 
+template <typename T>
+static void constructRandomMatrix(CoordinateMatrix<T> &matrix,
+                                  std::size_t size) {
+  matrix = CoordinateMatrix<T>(size);
+
+  std::random_device dev;
+  std::mt19937 rng(dev());
+  std::uniform_int_distribution<T> uniform;
+
+  for (std::size_t i = 0; i < size; i++) {
+    Coordinates newCoordinates = constructRandomCoordinates(0, size);
+    T randomValue = uniform(rng);
+    matrix.set(newCoordinates, randomValue);
+  }
+}
+
 static void BM_MultiKeyMatrix_Init(benchmark::State &state) {
   for (auto _ : state) {
     CoordinateMatrix<InnerMatrixType> matrix =
@@ -47,9 +41,13 @@ static void BM_MultiKeyMatrix_Init(benchmark::State &state) {
 
 static void BM_MultiKeyMatrix_Get(benchmark::State &state) {
   CoordinateMatrix<InnerMatrixType> matrix;
+  std::size_t lastSize = 0;
   for (auto _InitInit : state) {
     state.PauseTiming();
-    constructRandomMatrix(matrix, state.range());
+    if (lastSize != state.range()) {
+      constructRandomMatrix(matrix, state.range());
+      lastSize = state.range();
+    }
     Coordinates randomCoordinates =
         constructRandomCoordinates(0, state.range());
     state.ResumeTiming();
@@ -58,7 +56,9 @@ static void BM_MultiKeyMatrix_Get(benchmark::State &state) {
   }
 }
 
-// BENCHMARK(BM_MultiKeyMatrix_Init)->RangeMultiplier(2)->Range(2, 2 << 16);
-BENCHMARK(BM_MultiKeyMatrix_Get)->RangeMultiplier(2)->Range(2, 2 << 16)->Iterations(100);
+BENCHMARK(BM_MultiKeyMatrix_Init)->RangeMultiplier(2)->Range(2, 2 << 16);
+BENCHMARK(BM_MultiKeyMatrix_Get)->RangeMultiplier(2)->Range(2, 2 << 16)->Complexity();
+
+// TODO: set, remove, getByX, getByY
 
 BENCHMARK_MAIN();
