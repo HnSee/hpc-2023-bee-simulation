a130beaaef9b57ca446fb74b15a610cc2228f6e8
qub3s <g.eckardt@protonmail.com>
2023-06-27 11:21:26 +0200

    remove of wrong mpi folder

--- mpi/mpi_send_struct.cpp
+++ mpi/mpi_send_struct.cpp
@@ -40,21 +40,3 @@ int main (int argc, char *argv[])
 
     MPI_Finalize();
 }
-    {
-        my_struct m;
-        m.a = '!';
-        m.b = 1234;
-        m.c = 5678;
-
-        MPI_Send(&m, sizeof(my_struct), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
-    }
-    else 
-    {
-        my_struct m;
-        MPI_Recv(&m, sizeof(my_struct), MPI_CHAR, 0, 0, MPI_COMM_WORLD, 
-                 MPI_STATUS_IGNORE);
-        printf("%c %d %d\n", m.a, m.b, m.c); 
-    }
-
-    MPI_Finalize();
-}
