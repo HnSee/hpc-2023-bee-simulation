6587e853e1cf650198482d43fd568ee9583d2fe4
qub3s <g.eckardt@protonmail.com>
2023-06-27 23:05:22 +0200

    hive collection

--- src/agents/hive.cpp
+++ src/agents/hive.cpp
@@ -16,22 +16,20 @@ void Hive::init(int totalbees, HiveBeeAccess *hstore) {
 
 // collecting the bees
 void Hive::move() {  
-  this->state.agents.range( pos, 0.1 );
   
-  /*
-  RangeResult<double, Agent> *result = 
+  RangeResult<double, Agent> *result = this->state.agents.range( pos, 0.1 );
   int size = result->size();
 
   for(int k = 0; k < size; k++){
     if( result->at(k).value->gettype() == AgentType::Bee ){
 
-      //std::shared_ptr<Agent> a = result->at(k).value;
-      //PointValue<double, Agent> p = PointValue<double, Agent>(  result->at(k).value->pos, a );
+      std::shared_ptr<Agent> a = result->at(k).value;
+      PointValue<double, Agent> p = PointValue<double, Agent>(  result->at(k).value->pos, a );
       //this->state.agents.removeByPointValue( p );
     }
     //
   }
-  */
+
 
   return;
 }
