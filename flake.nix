28ff934710514514c999ff317f75435b2ab0b2a5
HnSee <34213265+HnSee@users.noreply.github.com>
2023-08-25 10:35:20 +0200

    Add Nix flake for reproducible build

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Project as part of the course \"Practical Course on High-Performance Computing\" (summer term 2023).";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  # Needs sandbox to be disabled because of meson's wrap dependencies
+  # Can be disabled while building using:
+  #   nix build --no-sandbox
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        packages = {
+          bee-simulation = pkgs.stdenv.mkDerivation rec {
+            name = "bee-simulation";
+            src = self;
+
+            nativeBuildInputs = with pkgs; [
+              meson
+              ninja
+              pkg-config
+              cacert
+            ];
+
+            buildInputs = with pkgs; [
+              openmpi
+              fmt
+              cairomm
+            ];
+
+            mesonWrapMode = "default";
+          };
+        };
+
+        defaultPackage = packages.bee-simulation;
+      });
+}
