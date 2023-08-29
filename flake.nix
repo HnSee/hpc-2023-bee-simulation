3e454386a4f9f78e87e72e25c8e0e0b8a6b92f3d
HnSee <34213265+HnSee@users.noreply.github.com>
2023-08-29 12:04:01 +0200

    Add working Nix build

--- flake.nix
+++ flake.nix
@@ -4,14 +4,30 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  # Needs sandbox to be disabled because of meson's wrap dependencies
-  # Can be disabled while building using:
-  #   nix build --no-sandbox
-
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        spdlog_fix = final: prev: {
+          spdlog = prev.spdlog.overrideAttrs (old: {
+            version = "1.12.0";
+            src = final.fetchFromGitHub {
+              owner = "gabime";
+              repo = "spdlog";
+              rev = "v1.12.0";
+              hash = "sha256-cxTaOuLXHRU8xMz9gluYz0a93O0ez2xOxbloyc1m1ns=";
+            };
+            patches = [
+              # Fix a broken test
+              (final.fetchpatch {
+                url = "https://github.com/gabime/spdlog/commit/2ee8bac78e6525a8ad9a9196e65d502ce390d83a.patch";
+                hash = "sha256-L79yOkm3VY01jmxNctfneTLmOA5DEQeNNGC8LbpJiOc=";
+              })
+            ];
+            propagatedBuildInputs = [ final.fmt ];
+            checkInputs = [ final.catch2_3 ];
+          });
+        };
+        pkgs = nixpkgs.legacyPackages.${system}.extend spdlog_fix;
       in
       rec {
         packages = {
@@ -23,18 +39,16 @@
               meson
               ninja
               pkg-config
-              cacert
             ];
 
             buildInputs = with pkgs; [
-              openmpi
-              fmt
               cairomm
               cxxopts
+              gbenchmark
+              gtest
+              openmpi
               spdlog
             ];
-
-            mesonWrapMode = "default";
           };
         };
 
