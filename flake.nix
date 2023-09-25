28b93d373758503dd7c2a78d9091b9bf9604aaf7
HnSee <34213265+HnSee@users.noreply.github.com>
2023-09-25 13:22:16 +0200

    Update Nix flake inputs to include newest spdlog patch

--- flake.nix
+++ flake.nix
@@ -7,38 +7,11 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        # spdlog version 1.11.x is not compatible with its dependency
-        # fmt version 10.x but 9.x, but upstream Nix fmt is version 10.x
-        # and spdlog is version 1.11.x (same problem on e.g. Arch Linux).
-        # spdlog version 1.12.x brings compatibility with fmt 10.x but is
-        # not upgraded in nixpkgs yet, see
-        # https://github.com/NixOS/nixpkgs/pull/250435.
-        # That is why a override to upgrade spdlog is needed here:
-        spdlog_fix = final: prev: {
-          spdlog = prev.spdlog.overrideAttrs (old: {
-            version = "1.12.0";
-            src = final.fetchFromGitHub {
-              owner = "gabime";
-              repo = "spdlog";
-              rev = "v1.12.0";
-              hash = "sha256-cxTaOuLXHRU8xMz9gluYz0a93O0ez2xOxbloyc1m1ns=";
-            };
-            patches = [
-              # Fix a broken test
-              (final.fetchpatch {
-                url = "https://github.com/gabime/spdlog/commit/2ee8bac78e6525a8ad9a9196e65d502ce390d83a.patch";
-                hash = "sha256-L79yOkm3VY01jmxNctfneTLmOA5DEQeNNGC8LbpJiOc=";
-              })
-            ];
-            propagatedBuildInputs = [ final.fmt ];
-            checkInputs = [ final.catch2_3 ];
-          });
-        };
-        pkgs = nixpkgs.legacyPackages.${system}.extend spdlog_fix;
+        pkgs = nixpkgs.legacyPackages.${system};
       in
       rec {
         packages = {
-          bee-simulation = pkgs.stdenv.mkDerivation rec {
+          bee-simulation = pkgs.stdenv.mkDerivation {
             name = "bee-simulation";
             src = self;
 
@@ -58,7 +31,7 @@
             ];
           };
           
-          bee-simulation-cmake = pkgs.stdenv.mkDerivation rec {
+          bee-simulation-cmake = pkgs.stdenv.mkDerivation {
             name = "bee-simulation-cmake";
             src = self;
 
